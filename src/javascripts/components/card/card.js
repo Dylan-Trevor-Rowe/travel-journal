e508c047f1db13ec2b655b78e63040cbb42c9054
Dylan Trevor Rowe <Dylanthk@gmail.com>
2020-05-11 13:48:10 -0500

    new

--- src/javascripts/components/card/card.js
+++ src/javascripts/components/card/card.js
@@ -8,14 +8,16 @@ const cityCard = () => {
   const cities = travelData.getCities();
   cities.forEach((e) => {
     domstring += `<div class="card">
+    <div class = "backgrround-color">
     <img class="card-img-top">
     <div class="card-body">
       <h5 class="card-title">${e.title}</h5>
       <img src= "${e.imageUrl}">
       <p class="card-text">${e.description}</p>
         <div class="input-group">
-        <button type="button" class="btn btn-info">submit</button>
+          <button type="button" class="submit">submit</button>
           <textarea name="" id="" class="submit-text" cols="25" rows="8"></textarea>
+        </div>
         </div>
           </div>
         </div>`;
